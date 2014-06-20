5504e87c66e2a1f9aa48f60421005aadbdc3e2ed
Andrei Formiga <archimedes_siracusa@hotmail.com>
2014-06-20 00:06:00 -0300

    Small formatting changes

--- src/matlibNative.ml
+++ src/matlibNative.ml
@@ -7,6 +7,7 @@ struct
 
   type matrix = { rows: int; cols: int; mxs: float array }
 
+  (* vector operations *)
   module Vector : VectorOps with type elem := float and type vector := vector = 
   struct 
     let zero n = { size = n; vxs = Array.create n 0.0 }
@@ -49,6 +50,7 @@ struct
       v.vxs |> Array.fold_left (fun s x -> s +. (x *. x)) 0.0 |> sqrt
   end
 
+  (* matrix operations *)
   module Matrix : MatrixOps with type elem := float 
                              and type vector := vector 
                              and type matrix := matrix =
@@ -105,62 +107,4 @@ struct
         res
   end
 
-end 
-
-(* 
-(* row-major native matrix *)
-module NativeMatrix : Matrix with type elem := float and type vec := NativeVector.t = 
-struct
-  type t = { rows: int; cols: int; elems: float array }
-
-  let create ?(initval=0.0) ~rows ~cols = 
-    { rows; cols; elems = Array.create (rows * cols) initval }
-
-  let zero rows cols = create rows cols 
-  
-  let from_array a ~rows ~cols = (* verify if there *)
-    if Array.length a <> (rows * cols) then failwith "from_array: Wrong dimensions for array"
-    else { rows; cols; elems = a } 
-
-  let init ~f ~rows ~cols = 
-    { rows; cols; elems = Array.init (rows * cols) (fun i -> f (i mod cols) (i / cols)) }
-
-  let rows m = m.rows 
-
-  let cols m = m.cols 
-
-  let get m row col = m.elems.(row * m.cols + col)
-
-  let add m1 m2 = (* check dimensions *)
-    let elems = m1.elems |> Array.mapi (fun i x -> x +. m2.elems.(i)) in
-    { rows = m1.rows; cols = m1.cols; elems }
-
-  let mult m1 m2 = 
-    if m1.cols <> m2.rows then
-      failwith "mult: Incompatible matrix dimensions"
-    else
-      let res = create m1.rows m2.cols in
-      for i = 0 to res.rows - 1 do
-        for j = 0 to res.cols - 1 do
-          for p = 0 to m1.cols - 1 do
-            res.elems.(i * res.cols + j) <- 
-              res.elems.(i * res.cols + j) +. m1.elems.(i * m1.cols + p) *. m2.elems.(p * m2.cols + j)
-          done
-        done
-      done;
-      res
-
-    let mult_vec m v = 
-      if m.cols <> NativeVector.size v then
-        failwith "mult_vec: Incompatible dimensions"
-      else
-        let res = NativeVector.zero m.cols in
-        for i = 0 to m.rows - 1 do
-          for j = 0 to m.cols - 1 do
-            NativeVector.set res i 
-              ((NativeVector.get res i) +. (NativeVector.get v j) *. m.elems.(i * m.cols + j))
-          done
-        done;
-        res
 end
-*)
